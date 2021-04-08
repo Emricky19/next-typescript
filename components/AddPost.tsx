25fb2591b8ebe04a33cbd5a6fb9f64d643c84f75
Emrick19 <emricky17@gmail.com>
2021-04-08 23:35:14 +0100

    Create AddPost component

---
+++ components/AddPost.tsx
@@ -0,0 +1,42 @@
+import React,  {useState} from 'react'
+import { IPost } from '../types'
+
+type Props = {
+    savePost: (e: React.FormEvent, formData: IPost) => void
+}
+
+
+const AddPost = ({savePost} : Props) => {
+    const [ formData, setFormData ] = useState<IPost>()
+
+    const handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+        const { id, value } = e.currentTarget
+
+        setFormData({
+            ...formData,
+            [id]: value
+        })
+    }
+    return (
+        <form className='Form' onSubmit={(e) => savePost(e, formData)}>
+        <div>
+          <div className='Form--field'>
+            <label htmlFor='name'>Title</label>
+            <input onChange={handleChange} type='text' id='title' />
+          </div>
+          <div className='Form--field'>
+            <label htmlFor='body'>Description</label>
+            <input onChange={handleChange} type='text' id='body' />
+          </div>
+        </div>
+        <button
+          className='Form__button'
+          disabled={formData === undefined ? true : false}
+        >
+          Add Post
+        </button>
+      </form>
+    )
+}
+
+export default AddPost
